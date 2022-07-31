1bedd25923481e18fc1339a5df7fe8d7bf4a275a
dfsdanimaulana <danimaulana9f@gmail.com>
2022-07-31 14:13:54 +0700

    login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home"
+import Login from "./pages/Login"
 import Product from "./pages/Product"
 import ProductList from "./pages/ProductList"
+import Register from "./pages/Register"
 
 export default function App() {
     return <div>
-      <Product />
+      <Login />
     </div>
 }
 
---
+++ src/pages/Register.jsx
@@ -0,0 +1,68 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        rgba(255, 255, 255, 0.5),
+        rgba(255, 255, 255, 0.5)
+        ),
+        url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940") center !important;
+    display: flex;
+    background-size: cover;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+    padding: 20px;
+    width: 40%;
+    background-color: white;
+`
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0 0;
+    padding: 10px;
+`
+const Agreement = styled.p`
+    font-size: 12px;
+    margin: 20px 0;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+`
+
+export default function Register() {
+  return (
+    <Container>
+        <Wrapper> 
+            <Title>Create An Account</Title>
+            <Form>
+                <Input placeholder="Name"/>
+                <Input placeholder="Last Name"/>
+                <Input placeholder="Username"/>
+                <Input placeholder="Email"/>
+                <Input placeholder="Password"/>
+                <Input placeholder="Confirm Password"/>
+                <Agreement>
+                    By creating an account, i consent to the processing of my personal data in accordance with te <b>PRIVACY POLICY</b>
+                </Agreement>
+                <Button>Register</Button>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
